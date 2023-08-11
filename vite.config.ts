abec2d41cb8605cb43ef99472dd506b3b86e17ae
Zachary Weston <zachary.weston@propellerhead.co.nz>
2023-08-11 16:58:24 +1200

    tidied viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,16 @@
-// <reference types="vitest" />
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-
-type Port = {
-  host: true
-  port: 3000
-}
-
-const port: Port = {
+const port = {
   host: true,
   port: 3000
-}
+};
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // test: {
-  //   globals: true,
-  //   environment: 'happy-dom'
-  // },
   server: port,
   preview: port,
   resolve: {
-    alias: [{ find: '@', replacement: '/src' }],
+    alias: [{ find: "@", replacement: "/src" }],
   },
-})
+}); 
